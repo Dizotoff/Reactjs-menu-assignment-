3a81a715bf4ecb9d4d69aa3205d90d3d6047c5b8
Dizotoff <izotov.dima@gmail.com>
2019-02-23 16:18:05 +0200

    mobile version update

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -9,10 +9,15 @@ const toolbar = props =>  {
         <nav className="toolbar__navigation">
             <div className="toolbar__logo"><a href='/'><img src={logo}  alt="telia logo"/></a></div>
             <div className="spacer"/>
+            <div className="toolbar__mobile">
+                <DrawerToggleButtton click={props.drawerClickHandler} check={props.open}/> 
+                <a href="/"><img src={pin}  alt="menu icon"/></a> 
+                <a href="/"><img src={pin}  alt="menu icon"/></a>
+            </div>
             <div className="toolbar__navigation-items">
                 <ul>
                     <li><DrawerToggleButtton click={props.drawerClickHandler} check={props.open} /></li>
-                    <li><a href="/"><img src={pin}  alt="menu icon"/></a><a href="/">LOGIN</a></li>
+                    <li><a href="/"><img src={pin}  alt="menu icon"/></a><a className="toolbar-login"href="/">LOGIN</a></li>
                     <li><a href="/"><img src={pin}  alt="menu icon"/></a></li>
                 </ul>
             </div>
