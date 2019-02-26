4ccb6dc69256c1952871f70c1d8af37e36f7f2f7
dizotoff <izotov.dima@gmail.com>
2019-02-26 15:28:33 +0200

    Updates

---
+++ precache-manifest.3e56bdc5a56582637875a8046cc93556.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "c83e61972b202702e88c32d44b91216c",
+    "url": "/Reactjs-menu-assignment-/static/media/team.c83e6197.svg"
+  },
+  {
+    "revision": "8c1029fcb2ba087ef48d",
+    "url": "/Reactjs-menu-assignment-/static/css/main.ee37fa40.chunk.css"
+  },
+  {
+    "revision": "a333c7a87cc51a1f02db",
+    "url": "/Reactjs-menu-assignment-/static/js/runtime~main.a333c7a8.js"
+  },
+  {
+    "revision": "58b350bde1fba71176eb",
+    "url": "/Reactjs-menu-assignment-/static/js/2.58b350bd.chunk.js"
+  },
+  {
+    "revision": "de41ed2c8be8c8a59dc68809cf92d25e",
+    "url": "/Reactjs-menu-assignment-/static/media/shopping-cart.de41ed2c.svg"
+  },
+  {
+    "revision": "56902ef6be3c342a922ca827271f8c88",
+    "url": "/Reactjs-menu-assignment-/static/media/user-silhouette.56902ef6.svg"
+  },
+  {
+    "revision": "bb93e3a2bf87cd2035f1215f4bea8aa3",
+    "url": "/Reactjs-menu-assignment-/static/media/cross-symbol.bb93e3a2.svg"
+  },
+  {
+    "revision": "8df8445051631f0638250163756bd171",
+    "url": "/Reactjs-menu-assignment-/static/media/musica-searcher.8df84450.svg"
+  },
+  {
+    "revision": "c980b8c56952cfab92c21340ee6e7e31",
+    "url": "/Reactjs-menu-assignment-/static/media/liiga-logo_128x128.c980b8c5.png"
+  },
+  {
+    "revision": "5b316389a9bde74262f76de12e3203c0",
+    "url": "/Reactjs-menu-assignment-/static/media/pin.5b316389.svg"
+  },
+  {
+    "revision": "e22430b933457d2c83aad89b0167446d",
+    "url": "/Reactjs-menu-assignment-/static/media/chat.e22430b9.svg"
+  },
+  {
+    "revision": "8c1029fcb2ba087ef48d",
+    "url": "/Reactjs-menu-assignment-/static/js/main.8c1029fc.chunk.js"
+  },
+  {
+    "revision": "d5d7383aa999881f3cb98372b597dd20",
+    "url": "/Reactjs-menu-assignment-/static/media/smartphones-couple.d5d7383a.svg"
+  },
+  {
+    "revision": "8a3f1dac962d6309be6f264ac76bd600",
+    "url": "/Reactjs-menu-assignment-/static/media/worlwide-transmission.8a3f1dac.svg"
+  },
+  {
+    "revision": "1e00491a9632f57a790ad1e46fe80832",
+    "url": "/Reactjs-menu-assignment-/static/media/question-mark-on-a-circular-black-background.1e00491a.svg"
+  },
+  {
+    "revision": "161b9e1478d3a325deb1a2fbf89762bb",
+    "url": "/Reactjs-menu-assignment-/static/media/simcard.161b9e14.svg"
+  },
+  {
+    "revision": "257663567a8a17ef71a5c714fbf0321d",
+    "url": "/Reactjs-menu-assignment-/static/media/shopping-bag.25766356.svg"
+  },
+  {
+    "revision": "e16aca569893abae94f0c32a3addb973",
+    "url": "/Reactjs-menu-assignment-/static/media/big-and-small-dots.e16aca56.svg"
+  },
+  {
+    "revision": "0e2b708f029bba378c29080d2c881da5",
+    "url": "/Reactjs-menu-assignment-/static/media/monitor.0e2b708f.svg"
+  },
+  {
+    "revision": "b24534a93c9e359eca921ff8068770b2",
+    "url": "/Reactjs-menu-assignment-/static/media/logo.b24534a9.svg"
+  },
+  {
+    "revision": "3553455173ea2699bdf54ddaef36480f",
+    "url": "/Reactjs-menu-assignment-/static/media/303D52_0_0.35534551.woff2"
+  },
+  {
+    "revision": "f5863c6aa408fe20acd924f5e5c77f0c",
+    "url": "/Reactjs-menu-assignment-/static/media/Pebble_W_Rg.f5863c6a.woff2"
+  },
+  {
+    "revision": "af1da8ba345beaa35261f6e1f20ea74b",
+    "url": "/Reactjs-menu-assignment-/index.html"
+  }
+];
